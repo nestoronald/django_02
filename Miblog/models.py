2462bafd95e353d9cadfcfc2b14b6bb074e6a1a3
Ronald Nestor <ronald4261@hotmail.com>
2013-11-08 17:36:43 -0500

    primer repo

---
+++ Miblog/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+class Articulos(models.Model)
+	author = models.CharField(max_length = 30)
+	titulo = models.CharField(max_length = 30)
+	texto = models.TextField()
+	fecha = models.DateTimeField()
+	
---
+++ Miblog/views.py
@@ -0,0 +1,12 @@
+# Create your views here.
+from django.shortcuts import render_to_response
+from Miblog.models import Articulos
+def home(request):
+	#contenido = {
+	#	'titulo' : 'Mi primer Articulo',
+	#	'author' : 'Ronald Loyola',
+	#	'fecha' : '08 de Octubre 2013',
+	#	'contenido' : 'aqui va el contenido ....',
+	#}
+	entradas = Articulos.objects.all()[:10]
+	return render_to_response('index.html',contenido)
