a610785d54c3c0955d747e8ed62f2c1ec902ccad
Ronald_casa_LMD <ronald4261@hotmail.com>
2013-11-15 01:28:02 -0500

    ---

--- Miblog/models.py
+++ Miblog/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 
 # Create your models here.
-class Articulos(models.Model)
+class Articulos(models.Model):
 	author = models.CharField(max_length = 30)
-	titulo = models.CharField(max_length = 30)
+	titulo = models.CharField(max_length = 100)
 	texto = models.TextField()
 	fecha = models.DateTimeField()
 	
--- Miblog/views.py
+++ Miblog/views.py
@@ -2,11 +2,12 @@
 from django.shortcuts import render_to_response
 from Miblog.models import Articulos
 def home(request):
-	#contenido = {
-	#	'titulo' : 'Mi primer Articulo',
-	#	'author' : 'Ronald Loyola',
-	#	'fecha' : '08 de Octubre 2013',
-	#	'contenido' : 'aqui va el contenido ....',
-	#}
+	# contenido = {
+	# 	'titulo' : 'Mi primer Articulo',
+	# 	'author' : 'Ronald Loyola',
+	# 	'fecha' : '08 de Octubre 2013',
+	# 	'contenido' : 'aqui va el contenido ....',
+	# }
 	entradas = Articulos.objects.all()[:10]
-	return render_to_response('index.html',contenido)
+	# return render_to_response('index.html',contenido)
+	return render_to_response('index.html',{'articulos' : entradas})	
